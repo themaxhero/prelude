64537c110169371ca31f8dd0030dcebf739cc67a
kress <cferraz95@gmail.com>
2020-08-23 05:45:17 -0300

    kind abstraction

---
+++ kind.ts
@@ -0,0 +1,20 @@
+export interface kind<B = unknown, A = unknown> {
+  _: A;
+  $: B;
+};
+
+export default kind;
+
+export type _ = "_";
+
+export type $ = "$";
+
+export type ap<T extends kind, A = unknown> = (T & { _: A })[$];
+
+export interface flip<T extends kind<kind>> extends kind {
+  $: FlipKind<T, this[_]>
+}
+
+interface FlipKind<T extends kind<kind, A>, A> extends kind {
+  $: ap<ap<T, this[_]>, A>
+}
