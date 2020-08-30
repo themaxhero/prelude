975a75ae633ad015d09553cbc9da68a0a2ea6db0
kress <cferraz95@gmail.com>
2020-08-30 04:55:53 -0300

    monoid laws

--- data/monoid.ts
+++ data/monoid.ts
@@ -1,7 +1,25 @@
 import Semigroup from "./semigroup.ts";
+import { AssertEquals } from "../test/asserts.ts";
 
 export interface Monoid<T> extends Semigroup<T> {
   empty: () => T;
 }
 
 export default Monoid;
+
+export const assertMonoid = <T>(
+  { concat, empty, assertEquals, a }:
+    & Monoid<T>
+    & { assertEquals: AssertEquals; a: T },
+) => {
+  assertEquals(
+    concat(a, empty()),
+    a,
+    "monoid right identity law",
+  );
+  assertEquals(
+    concat(empty(), a),
+    a,
+    "monoid left identity law",
+  );
+};
