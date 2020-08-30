a7a07ef88b05f5ef128dfa2c6fd835c814cdc430
kress <2013206+kress95@users.noreply.github.com>
2020-08-30 05:15:58 -0300

    category laws

--- control/category.ts
+++ control/category.ts
@@ -1,8 +1,29 @@
 import { Kind2, Ap2 } from "../kind.ts";
 import Semigroupoid from "../control/semigroupoid.ts";
+import { AssertEquals } from "../test/asserts.ts";
 
 export interface Category<T extends Kind2> extends Semigroupoid<T> {
   id: <I, J>() => Ap2<T, I, J>;
 }
 
 export default Category;
+
+export const assertCategory = <T extends Kind2, I, J, K>(
+  args:
+    & Category<T>
+    & { assertEquals: AssertEquals; a: I },
+) => {
+  const { id, compose, assertEquals, a } = args;
+
+  assertEquals(
+    compose<I, I, I>(a, id<I, I>()),
+    a,
+    "category right identity law",
+  );
+
+  assertEquals(
+    compose<I, I, I>(id<I, I>(), a),
+    a,
+    "category left identity law",
+  );
+};
