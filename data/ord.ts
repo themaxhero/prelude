ce052f9862673a3b7647e806db11d56f0eb0ffe3
kress <cferraz95@gmail.com>
2020-08-30 04:55:32 -0300

    ord laws

--- data/ord.ts
+++ data/ord.ts
@@ -1,7 +1,29 @@
-import Setoid from "./setoid.ts";
+import Setoid, { assertSetoid } from "./setoid.ts";
+import { Assert } from "../test/asserts.ts";
 
 export interface Ord<T> extends Setoid<T> {
   lte: (x: T, y: T) => boolean;
 }
 
 export default Ord;
+
+export const assertOrd = <T>(
+  args: Ord<T> & { assert: Assert; a: T; b: T; c: T },
+) => {
+  assertSetoid(args);
+
+  const { lte, equals, assert, a, b, c } = args;
+
+  assert(
+    lte(a, b) || lte(b, a),
+    "ord totality law",
+  );
+  assert(
+    lte(a, b) && lte(b, a) ? equals(a, b) : !equals(a, b),
+    "ord antisymmetry law",
+  );
+  assert(
+    lte(a, b) && lte(b, c) ? lte(a, c) : lte(a, c),
+    "ord transitivity law",
+  );
+};
