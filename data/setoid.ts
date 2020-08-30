0897a13bd40aeadbc4b3b2b62d8d8040d0aca1b2
kress <cferraz95@gmail.com>
2020-08-30 04:55:01 -0300

    setoid laws

--- data/setoid.ts
+++ data/setoid.ts
@@ -1,5 +1,27 @@
+import { Assert } from "../test/asserts.ts";
+
 export interface Setoid<T> {
   equals: (x: T, y: T) => boolean;
 }
 
 export default Setoid;
+
+export const assertSetoid = <T>(
+  { equals, assert, a, b, c }:
+    & Setoid<T>
+    & { assert: Assert; a: T; b: T; c: T },
+) => {
+  assert(
+    equals(a, a),
+    "setoid reflexivity law",
+  );
+
+  assert(
+    equals(a, b) === equals(b, a),
+    "setoid symmetry law",
+  );
+  assert(
+    equals(a, b) && equals(b, c) ? equals(a, c) : !equals(a, c),
+    "setoid transitivity law",
+  );
+};
