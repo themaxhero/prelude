7a3d2ce88f749327580152a7a1c699d42398d221
kress <cferraz95@gmail.com>
2020-08-30 04:56:06 -0300

    semigroup laws

--- data/semigroup.ts
+++ data/semigroup.ts
@@ -1,5 +1,19 @@
+import { AssertEquals } from "../test/asserts.ts";
+
 export interface Semigroup<T> {
   concat: (x: T, y: T) => T;
 }
 
 export default Semigroup;
+
+export const assertSemigroup = <T>(
+  { concat, assertEquals, a, b, c }:
+    & Semigroup<T>
+    & { assertEquals: AssertEquals; a: T; b: T; c: T },
+) => {
+  assertEquals(
+    concat(concat(a, b), c),
+    concat(a, concat(b, c)),
+    "semigroup associativity law",
+  );
+};
