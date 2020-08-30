337542af52187e3b86ed6393f8365a8b2c5f438e
kress <2013206+kress95@users.noreply.github.com>
2020-08-30 05:05:54 -0300

    group laws

--- data/group.ts
+++ data/group.ts
@@ -1,7 +1,28 @@
-import Monoid from "./monoid.ts";
+import Monoid, { assertMonoid } from "./monoid.ts";
+import { AssertEquals } from "../test/asserts.ts";
 
 export interface Group<T> extends Monoid<T> {
   invert: (x: T) => T;
 }
 
 export default Group;
+
+export const assertGroup = <T>(
+  args: Group<T> & { assertEquals: AssertEquals; a: T; b: T; c: T },
+) => {
+  assertMonoid(args);
+
+  const { invert, empty, concat, assertEquals, a } = args;
+
+  assertEquals(
+    concat(a, invert(a)),
+    empty(),
+    "group right inverse law",
+  );
+
+  assertEquals(
+    empty(),
+    concat(a, invert(a)),
+    "group left inverse law",
+  );
+};
