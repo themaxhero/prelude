b3f91d2b75475514ec8fd6f001e2f6680048e070
kress <2013206+kress95@users.noreply.github.com>
2020-08-30 19:22:27 -0300

    functor laws

--- data/functor.ts
+++ data/functor.ts
@@ -1,4 +1,5 @@
 import Kind, { Ap } from "../kind.ts";
+import { AssertEquals } from "../test/asserts.ts";
 
 export interface Functor<T extends Kind> {
   map: <A, B>(
@@ -8,3 +9,20 @@ export interface Functor<T extends Kind> {
 }
 
 export default Functor;
+
+export const testFunctor = <T extends Kind, A, B, C>(
+  { map, assertEquals, a, f, g }:
+    & Functor<T>
+    & { assertEquals: AssertEquals; a: A; f: (c: B) => C; g: (a: A) => B },
+) => {
+  assertEquals(
+    map<A, A>((x: A) => x, a),
+    a,
+    "functor identity law",
+  );
+  assertEquals(
+    map<A, C>((x: A) => f(g(x)), a),
+    map<B, C>(f, map<A, B>(g, a)),
+    "functor composition law",
+  );
+};
