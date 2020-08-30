481b243002d8021b493404dbc62a776ffa54c985
kress <2013206+kress95@users.noreply.github.com>
2020-08-30 19:17:47 -0300

    filterable laws

--- data/filterable.ts
+++ data/filterable.ts
@@ -1,4 +1,5 @@
 import Kind, { Ap } from "../kind.ts";
+import { AssertEquals } from "../test/asserts.ts";
 
 export interface Filterable<T extends Kind> {
   filter: <A>(
@@ -8,3 +9,33 @@ export interface Filterable<T extends Kind> {
 }
 
 export default Filterable;
+
+export const testFilterable = <T extends Kind, A>(
+  { filter, assertEquals, a, b, f, g }:
+    & Filterable<T>
+    & {
+      assertEquals: AssertEquals;
+      f: (a: A) => boolean;
+      g: (a: A) => boolean;
+      a: A;
+      b: A;
+    },
+) => {
+  assertEquals(
+    filter<A>((x: A) => f(x) && g(x), a),
+    filter<A>(g, filter(f, a)),
+    "filterable distributivity identity law",
+  );
+
+  assertEquals(
+    filter<A>((a: A) => true, a),
+    a,
+    "filterable identity law",
+  );
+
+  assertEquals(
+    filter<A>((a: A) => false, a),
+    filter<A>((a: A) => false, b),
+    "filterable annihilation law",
+  );
+};
