0cceb70527ce0a97f631832dd8ba9b2abb31c469
kress <2013206+kress95@users.noreply.github.com>
2020-08-31 02:28:04 -0300

    profunctor laws

--- data/profunctor.ts
+++ data/profunctor.ts
@@ -1,4 +1,5 @@
 import { Kind2, Ap2 } from "../kind.ts";
+import { AssertEquals } from "../test/asserts.ts";
 
 export interface Profunctor<T extends Kind2> {
   promap: <A, B, C, D>(
@@ -9,3 +10,31 @@ export interface Profunctor<T extends Kind2> {
 }
 
 export default Profunctor;
+
+// TODO: derive Profunctor
+// map: (f, u) => promap(x => x, f, u)
+
+export const testProfunctor = <T extends Kind2, A, B, C, D, E, F>(
+  { promap, assertEquals, a, f, g, h, i }:
+    & Profunctor<T>
+    & {
+      assertEquals: AssertEquals;
+      a: Ap2<T, C, B>;
+      f: (b: B) => C;
+      g: (a: A) => B;
+      h: (e: E) => F;
+      i: (d: D) => E;
+    },
+) => {
+  assertEquals(
+    promap<A, A, B, B>((x: A): A => x, (x: B): B => x, a),
+    a,
+    "profunctor identity law",
+  );
+
+  assertEquals(
+    promap<A, C, D, F>((x: A) => f(g(x)), (x: D) => h(i(x)), a),
+    promap<A, B, E, F>(g, h, promap<B, C, D, E>(f, i, a)),
+    "profunctor composition law",
+  );
+};
