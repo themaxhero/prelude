224fa6b290e3c0b80735911a07d9f2f88ff5f592
kress <2013206+kress95@users.noreply.github.com>
2020-08-31 01:47:41 -0300

    bifunctor laws

--- data/bifunctor.ts
+++ data/bifunctor.ts
@@ -1,5 +1,6 @@
 import { Kind2, Ap, Ap2, Flip } from "../kind.ts";
-import Functor from "./functor.ts";
+import Functor, { testFunctor } from "./functor.ts";
+import { AssertEquals } from "../test/asserts.ts";
 
 export interface Bifunctor<T extends Kind2> {
   bimap: <A, B, C, D>(
@@ -7,8 +8,8 @@ export interface Bifunctor<T extends Kind2> {
     g: (x: C) => D,
     t: Ap2<T, A, C>,
   ) => Ap2<T, B, D>;
-  first: <A, B>(t: Ap2<T, A, B>) => Functor<Ap<Flip<T>, B>>;
-  second: <A, B>(t: Ap2<T, A, B>) => Functor<Ap<T, A>>;
+  first: <J>() => Functor<Ap<Flip<T>, J>>;
+  second: <I>() => Functor<Ap<T, I>>;
 }
 
 export default Bifunctor;
@@ -16,12 +17,66 @@ export default Bifunctor;
 export const deriveBifunctor = <T extends Kind2>(
   { bimap, ...rest }: Pick<Bifunctor<T>, "bimap">,
 ): Bifunctor<T> => ({
-  first: <A, B>(_t: Ap2<T, A, B>) => ({
-    map: (f, u) => bimap(f, (x) => x, u),
+  first: <J>(): Functor<Ap<Flip<T>, J>> => ({
+    map: <A, B>(f: (x: A) => B, u: Ap2<T, J, A>): Ap2<T, B, J> =>
+      bimap<A, B, A, A>(f, (x: A): A => x, u),
   }),
-  second: <A, B>(_t: Ap2<T, A, B>) => ({
-    map: (f, u) => bimap((x) => x, f, u),
+  second: <I>(): Functor<Ap<T, I>> => ({
+    map: <A, B>(f: (x: A) => B, u: Ap2<T, A, I>): Ap2<T, I, B> =>
+      bimap<A, A, A, B>((x: A): A => x, f, u),
   }),
   bimap,
   ...rest,
 });
+
+export const testBifunctor = <T extends Kind2, A, B, C, E, D, F, G>(
+  args:
+    & Bifunctor<T>
+    & {
+      assertEquals: AssertEquals;
+      a: Ap2<T, A, D>;
+      f: (c: B) => C;
+      g: (a: A) => B;
+      h: (e: F) => G;
+      i: (c: E) => F;
+    },
+) => {
+  const { first, second } = deriveBifunctor(args);
+  const { assertEquals, bimap, a, f, g, h, i } = args;
+
+  testFunctor<Ap<Flip<T>, D>, A, B, C>({
+    ...first<D>(),
+    assertEquals,
+    a,
+    g,
+    f,
+  });
+
+  testFunctor<Ap<T, A>, A, B, C>({
+    ...second<A>(),
+    assertEquals,
+    a,
+    g,
+    f,
+  });
+
+  assertEquals(
+    bimap((x: A) => x, (x: D) => x, a),
+    a,
+    "bifunctor identity law",
+  );
+
+  assertEquals(
+    bimap<A, C, E, G>(
+      (x: A) => f(g(x)),
+      (x: E) => h(i(x)),
+      a,
+    ),
+    bimap<B, C, F, G>(
+      f,
+      h,
+      bimap(g, i, a),
+    ),
+    "bifunctor composition law",
+  );
+};
