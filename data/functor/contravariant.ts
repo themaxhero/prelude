a83072328af10e68cdea1f88255562d52b12947f
kress <2013206+kress95@users.noreply.github.com>
2020-08-31 02:01:57 -0300

    contramap laws

--- data/functor/contravariant.ts
+++ data/functor/contravariant.ts
@@ -1,4 +1,5 @@
 import Kind, { Ap } from "../../kind.ts";
+import { AssertEquals } from "../../test/asserts.ts";
 
 export interface Contravariant<T extends Kind> {
   contramap: <A, B>(
@@ -8,3 +9,26 @@ export interface Contravariant<T extends Kind> {
 }
 
 export default Contravariant;
+
+export const testContravariant = <T extends Kind, A, B, C>(
+  { contramap, assertEquals, a, f, g }:
+    & Contravariant<T>
+    & {
+      assertEquals: AssertEquals;
+      a: Ap<T, A>;
+      f: (B: B) => C;
+      g: (a: A) => B;
+    },
+) => {
+  assertEquals(
+    contramap<A, A>((x: A): A => x, a),
+    a,
+    "contravariant identity law",
+  );
+
+  assertEquals(
+    contramap<A, C>((x: A) => f(g(x)), a),
+    contramap<A, B>(g, contramap<B, C>(f, a)),
+    "contravariant composition law",
+  );
+};
