f901c6f15bf5eeb05cc6314ff709dab3a1dc3683
kress <2013206+kress95@users.noreply.github.com>
2020-09-01 02:17:36 -0300

    foldable laws

--- data/foldable.ts
+++ data/foldable.ts
@@ -1,11 +1,34 @@
 import Kind, { Ap } from "../kind.ts";
+import { AssertEquals } from "../test/asserts.ts";
 
 export interface Foldable<T extends Kind> {
   reduce: <A, B>(
-    f: (x: A, y: B) => A,
-    x: A,
-    u: Ap<T, B>,
-  ) => A;
+    f: (x: B, y: A) => B,
+    x: B,
+    u: Ap<T, A>,
+  ) => B;
 }
 
 export default Foldable;
+
+export const testFoldable = <T extends Kind, A, B, C>(
+  { assertEquals, reduce, a, b, f }:
+    & Foldable<T>
+    & {
+      assertEquals: AssertEquals;
+      a: Ap<T, A>;
+      b: B;
+      f: (x: B, y: A) => B;
+    },
+) => {
+  assertEquals(
+    reduce(f, b, a),
+    (<A, B>(f: (x: A, y: B) => A, x: A, u: Ap<T, B>): A =>
+      reduce<B, B[]>(
+        (acc: B[], y: B) => acc.concat([y]),
+        [] as B[],
+        u,
+      ).reduce<A>(f, x))(f, b, a),
+    "foldable equivalence law",
+  );
+};
