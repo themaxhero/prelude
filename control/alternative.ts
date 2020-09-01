679ea5a433e9c3695ed4efded6263ffe7970c3d5
kress <2013206+kress95@users.noreply.github.com>
2020-09-01 01:39:48 -0300

    alternative laws

--- control/alternative.ts
+++ control/alternative.ts
@@ -1,7 +1,45 @@
-import Kind from "../kind.ts";
-import Applicative from "./applicative.ts";
-import Plus from "./plus.ts";
+import Kind, { Ap } from "../kind.ts";
+import Applicative, { testApplicative } from "./applicative.ts";
+import Plus, { testPlus } from "./plus.ts";
+import { AssertEquals } from "../test/asserts.ts";
 
 export interface Alternative<T extends Kind> extends Applicative<T>, Plus<T> {}
 
 export default Alternative;
+
+export const testAlternative = <T extends Kind, A, B, C>(
+  { d, h, i, ...args }:
+    & Alternative<T>
+    & {
+      assertEquals: AssertEquals;
+      a: Ap<T, A>;
+      b: Ap<T, A>;
+      c: Ap<T, A>;
+      d: Ap<T, (x: B) => A>;
+      u: Ap<T, (x: C) => B>;
+      v: Ap<T, C>;
+      f: (a: A) => B;
+      g: (b: B) => C;
+      h: (b: B) => A;
+      i: (c: C) => B;
+      x: B;
+      y: C;
+    },
+) => {
+  testApplicative<T, A, B, C>({ ...args, a: d, f: h, g: i });
+  testPlus<T, A, B, C>(args);
+
+  const { ap, alt, zero, assertEquals, a, b, c } = args;
+
+  assertEquals(
+    ap<A, A>(alt<A>(a, b), c),
+    alt<A>(ap<A, A>(a, c), ap<A, A>(b, c)),
+    "alternative distributivity law",
+  );
+
+  assertEquals(
+    ap<A, A>(zero<A>(), a),
+    zero<A>(),
+    "alternative annihilation law",
+  );
+};
