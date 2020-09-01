8d1f455f948f8c70e9ff8af5c97c8d9bc3b969dd
kress <2013206+kress95@users.noreply.github.com>
2020-08-31 21:04:14 -0300

    apply laws

--- control/apply.ts
+++ control/apply.ts
@@ -1,5 +1,6 @@
 import Kind, { Ap } from "../kind.ts";
-import Functor from "../data/functor.ts";
+import Functor, { testFunctor } from "../data/functor.ts";
+import { AssertEquals } from "../test/asserts.ts";
 
 export interface Apply<T extends Kind> extends Functor<T> {
   ap: <A, B>(
@@ -9,3 +10,33 @@ export interface Apply<T extends Kind> extends Functor<T> {
 }
 
 export default Apply;
+
+export const testApply = <T extends Kind, A, B, C>(
+  { map, ap, assertEquals, a, u, v, f, g }:
+    & Apply<T>
+    & {
+      assertEquals: AssertEquals;
+      a: Ap<T, (x: B) => A>;
+      u: Ap<T, (x: C) => B>;
+      v: Ap<T, C>;
+      f: (b: B) => A;
+      g: (c: C) => B;
+    },
+) => {
+  testFunctor<T, C, B, A>({ map, assertEquals, a: u, f, g });
+
+  assertEquals(
+    ap<C, A>(
+      ap<(x: C) => B, (x: C) => A>(
+        map<(x: B) => A, (g: (x: C) => B) => (x: C) => A>(
+          (f: (x: B) => A) => (g: (x: C) => B) => (x: C) => f(g(x)),
+          a,
+        ),
+        u,
+      ),
+      v,
+    ),
+    ap<B, A>(a, ap<C, B>(u, v)),
+    "apply composition law",
+  );
+};
