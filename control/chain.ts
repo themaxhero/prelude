b056b4b6eec72849db9e8cf031f1e2e0c97081cd
kress <2013206+kress95@users.noreply.github.com>
2020-09-01 01:57:13 -0300

    chain laws

--- control/chain.ts
+++ control/chain.ts
@@ -1,6 +1,7 @@
 import Kind, { Ap } from "../kind.ts";
-import Apply from "./apply.ts";
+import Apply, { testApply } from "./apply.ts";
 import Functor from "../data/functor.ts";
+import { AssertEquals } from "../test/asserts.ts";
 
 export interface Chain<T extends Kind> extends Apply<T> {
   chain: <A, B>(
@@ -21,3 +22,35 @@ export const deriveChain = <T extends Kind>(
   map,
   ...rest,
 });
+
+export const testChain = <T extends Kind, A, B, C>(
+  { map, chain, assertEquals, a, u, f, g, ...args }:
+    & Chain<T>
+    & {
+      assertEquals: AssertEquals;
+      a: Ap<T, (x: B) => A>;
+      u: Ap<T, A>;
+      v: Ap<T, (x: C) => B>;
+      w: Ap<T, C>;
+      f: (a: A) => B;
+      g: (b: B) => C;
+      h: (b: B) => A;
+      i: (c: C) => B;
+    },
+) => {
+  testApply<T, A, B, C>({
+    ...deriveChain({ map, chain }),
+    assertEquals,
+    a,
+    u: args.v,
+    v: args.w,
+    f: args.h,
+    g: args.i,
+  });
+
+  assertEquals(
+    chain(g, chain(f, u)),
+    chain((x: A) => chain(g, f(x)), u),
+    "chain associativity law",
+  );
+};
