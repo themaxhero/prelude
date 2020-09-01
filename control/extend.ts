00a52bd072e7233e0259b3367844f460afce3e11
kress <2013206+kress95@users.noreply.github.com>
2020-09-01 02:08:04 -0300

    extend laws

--- control/extend.ts
+++ control/extend.ts
@@ -1,5 +1,6 @@
 import Kind, { Ap } from "../kind.ts";
-import Functor from "../data/functor.ts";
+import Functor, { testFunctor } from "../data/functor.ts";
+import { AssertEquals } from "../test/asserts.ts";
 
 export interface Extend<T extends Kind> extends Functor<T> {
   extend: <A, B>(
@@ -9,3 +10,25 @@ export interface Extend<T extends Kind> extends Functor<T> {
 }
 
 export default Extend;
+
+export const assertExtend = <T extends Kind, A, B, C>(
+  args:
+    & Extend<T>
+    & {
+      assertEquals: AssertEquals;
+      a: Ap<T, A>;
+      w: Ap<T, A>;
+      f: (b: Ap<T, B>) => C;
+      g: (a: Ap<T, A>) => B;
+    },
+) => {
+  testFunctor<T, A, B, C>(args);
+
+  const { extend, assertEquals, f, g, w } = args;
+
+  assertEquals(
+    extend<A, C>(f, extend<A, B>(g, w)),
+    extend<A, C>((_: Ap<T, A>) => f(extend<A, B>(g, _)), w),
+    "extend left identity law",
+  );
+};
