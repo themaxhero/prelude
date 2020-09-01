d638c448efaecaeb58cdf1b7a04d4a7843ebe991
kress <2013206+kress95@users.noreply.github.com>
2020-09-01 01:12:43 -0300

    plus laws

--- control/plus.ts
+++ control/plus.ts
@@ -1,8 +1,44 @@
 import Kind, { Ap } from "../kind.ts";
-import Alt from "./alt.ts";
+import Alt, { testAlt } from "./alt.ts";
+import { AssertEquals } from "../test/asserts.ts";
 
 export interface Plus<T extends Kind> extends Alt<T> {
   zero: <A>() => Ap<T, A>;
 }
 
 export default Plus;
+
+export const testPlus = <T extends Kind, A, B, C>(
+  args:
+    & Plus<T>
+    & {
+      assertEquals: AssertEquals;
+      a: Ap<T, A>;
+      b: Ap<T, A>;
+      c: Ap<T, A>;
+      f: (a: A) => B;
+      g: (b: B) => C;
+    },
+) => {
+  testAlt<T, A, B, C>(args);
+
+  const { zero, alt, map, assertEquals, a, f } = args;
+
+  assertEquals(
+    alt<A>(a, zero<A>()),
+    a,
+    "plus right identity law",
+  );
+
+  assertEquals(
+    alt<A>(zero<A>(), a),
+    a,
+    "plus left identity law",
+  );
+
+  assertEquals(
+    map<A, B>(f, zero<A>()),
+    zero<A>(),
+    "plus annihilation law",
+  );
+};
