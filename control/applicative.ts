31505760ce334b3280a8397da45a335b1eb67ecf
kress <2013206+kress95@users.noreply.github.com>
2020-09-01 01:12:09 -0300

    applicative laws

--- control/applicative.ts
+++ control/applicative.ts
@@ -1,5 +1,6 @@
 import Kind, { Ap } from "../kind.ts";
-import Apply from "./apply.ts";
+import Apply, { testApply } from "./apply.ts";
+import { AssertEquals } from "../test/asserts.ts";
 
 export interface Applicative<T extends Kind> extends Apply<T> {
   of: <A>(x: A) => Ap<T, A>;
@@ -15,3 +16,40 @@ export const deriveApplicative = <T extends Kind>(
   of,
   ...rest,
 });
+
+export const testApplicative = <T extends Kind, A, B, C>(
+  args:
+    & Applicative<T>
+    & {
+      assertEquals: AssertEquals;
+      a: Ap<T, (x: B) => A>;
+      u: Ap<T, (x: C) => B>;
+      v: Ap<T, C>;
+      f: (b: B) => A;
+      g: (c: C) => B;
+      x: B;
+      y: C;
+    },
+) => {
+  testApply<T, A, B, C>(args);
+
+  const { of, ap, assertEquals, u, v, f, x, y } = args;
+
+  assertEquals(
+    ap<C, C>(of<(x: C) => C>((x: C): C => x), v),
+    v,
+    "applicative identity law",
+  );
+
+  assertEquals(
+    ap<B, A>(of<(b: B) => A>(f), of<B>(x)),
+    of<A>(f(x)),
+    "applicative homomorphism law",
+  );
+
+  assertEquals(
+    ap<C, B>(u, of<C>(y)),
+    ap<(x: C) => B, B>(of<(f: (c: C) => B) => B>((f: (c: C) => B) => f(y)), u),
+    "applicative interchange law",
+  );
+};
