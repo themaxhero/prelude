e55c9b2e62d5e951f9f972b369296f70106f984f
kress <2013206+kress95@users.noreply.github.com>
2020-09-08 04:02:25 -0300

    improve kind types

--- kind.ts
+++ kind.ts
@@ -3,26 +3,24 @@ export interface Kind<B = unknown, A = unknown> {
   $: B;
 }
 
-export type Kind2<A = unknown, B = unknown, C = unknown> = Kind<Kind<C, B>, A>;
-
 export default Kind;
 
-export type _ = "_";
+export type Kind2<A = unknown, B = unknown, C = unknown> = Kind<Kind<C, B>, A>;
 
-type $ = "$";
-
-export type Ap<T extends Kind, A = unknown> = (T & { _: A })[$];
+export type Ap<T extends Kind, A = unknown> = (T & { _: A })["$"];
 
 export type Ap2<T extends Kind2, A = unknown, B = unknown> = Ap<Ap<T, A>, B>;
 
+export interface Compose<A extends Kind2, B extends Kind> extends Kind {
+  $: Ap<A, Ap<B, this["_"]>>;
+}
+
 export interface Flip<T extends Kind2> extends Kind {
-  $: FlipKind<T, this[_]>;
+  $: FlipKind<T, this["_"]>;
 }
 
 interface FlipKind<T extends Kind2<A>, A> extends Kind {
-  $: Ap2<T, this[_], A>;
+  $: Ap2<T, this["_"], A>;
 }
 
-export interface Compose<A extends Kind2, B extends Kind> extends Kind {
-  $: Ap<A, Ap<B, this[_]>>;
-}
+export type _ = "_";
